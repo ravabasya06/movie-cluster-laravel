3cf54ee2190620548cce1784e6778dc423f5d048
Adreon <sachioramzy12@gmail.com>
2024-09-24 10:58:02 +0700

    add page layout

---
+++ resources/js/Components/Layout.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { watch } from "vue";
+import Nav from "./Nav.vue";
+import Footer from "./Footer.vue";
+const props = defineProps(["title"]);
+
+watch(
+    () => props.title,
+    (newTitle) => {
+        document.title = newTitle;
+    },
+    { immediate: true }
+);
+</script>
+
+<template>
+    <main>
+        <header>
+            <Nav />
+        </header>
+        <article>
+            <slot />
+        </article>
+        <footer>
+            <Footer />
+        </footer>
+    </main>
+</template>
+
+<style scoped>
+article {
+    display: flex;
+    flex-direction: column;
+    min-height: calc(100vh);
+}
+</style>

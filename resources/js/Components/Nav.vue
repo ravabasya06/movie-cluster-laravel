fb79f1300e9295a5a55b03fbb834e4f7c9eee44f
Adreon <sachioramzy12@gmail.com>
2024-09-24 10:58:36 +0700

    styling and adding search

---
+++ resources/js/Components/Nav.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Search from "./Search.vue";
+</script>
+
+<template>
+    <nav class="navbar">
+        <div class="left-group">
+            <Link href="/">
+                <img
+                    class="logo"
+                    src="../../../public/images/Logo-Movclus.png"
+                    alt="logo"
+                />
+            </Link>
+        </div>
+        <div class="right-group">
+            <Search />
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center; /* Align items vertically in the center */
+    position: sticky;
+    background-color: black;
+    padding: 10px;
+    z-index: 1;
+    padding: 0;
+}
+.logo {
+    padding: 15px;
+    width: 125px;
+}
+
+.left-group {
+    display: flex;
+}
+
+.right-group {
+    display: flex;
+}
+</style>

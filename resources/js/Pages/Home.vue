8b1a00fb6f2a8f496714c477c16b785683b682ff
Adreon <sachioramzy12@gmail.com>
2024-09-24 14:07:57 +0700

    showing the movie cards

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,19 @@
 <script setup>
 import Layout from "../Components/Layout.vue";
+import Cards from "../Components/Home/Cards.vue";
 </script>
 <template>
     <Layout title="Home">
-        <h1>asd</h1>
+        <h3>For you</h3>
+        <Cards />
+        <div class="genres">
+            <h3>Jenis Genre</h3>
+            <Cards />
+        </div>
     </Layout>
 </template>
-<style scoped></style>
+<style scoped>
+h3 {
+    margin: 30px;
+}
+</style>

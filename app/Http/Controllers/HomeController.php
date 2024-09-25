91b767b01b4981ce48745b79fb4c36beeea017ba
ravabasya06 <ravabasya06@gmail.com>
2024-09-25 17:26:49 +0700

    added trending movies

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,9 +10,13 @@ class HomeController extends Controller
 {
     public function index(){
         $key = config('services.tmdb.key');
-        $popular_movies = Http::get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key='.$key)->json();
+        $popular_movies = Http::get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key='.$key)->json();
+        $top_movies = Http::get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&api_key='.$key)->json();
+        $trending_movies = Http::get('https://api.themoviedb.org/3/trending/movie/day?include_adult=false&include_video=false&language=en-US&page=1&api_key='.$key)->json();
         return Inertia::render('Home', [
             'popular_movies' => $popular_movies,
+            'top_movies' => $top_movies,
+            'trending_movies' => $trending_movies,
         ]);
     }
 }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,15 +2,13 @@
 import Layout from "../Components/Layout.vue";
 import Cards from "../Components/Home/Cards.vue";
 
-defineProps(["popular_movies"]);
+defineProps(["popular_movies", "top_movies", "trending_movies"]);
 </script>
 <template>
     <Head title="Home" />
     <Layout>
-        <Cards title="Popular Movies" :movies="popular_movies" />
-        <div class="genres">
-            <h3>Jenis Genre</h3>
-        </div>
+        <Cards title="Popular Today" :movies="popular_movies" />
+        <Cards title=" Top Rated of All Time" :movies="top_movies" />
+        <Cards title="Trending Today" :movies="trending_movies" />
     </Layout>
 </template>
-<style scoped></style>
